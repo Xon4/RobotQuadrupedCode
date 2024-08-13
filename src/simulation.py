ebbdc534390688f1cfb96fd37154c8697022b3dd
Andrew Cao <xoncao@gmail.com>
2024-08-13 14:50:34 -0700

    fixed leg elongation issue

--- src/simulation.py
+++ src/simulation.py
@@ -2,10 +2,11 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import time
 
 #declare constants
-STEP_LENGTH = 100
-STEP_HEIGHT = 60
+STEP_LENGTH = 50
+STEP_HEIGHT = 30
 BACK_STEP_DEPTH = 10
 GROUND_DEPTH = 150
 GAIT_SPEED = 100
@@ -29,9 +30,11 @@ class Trajectory:
     def __init__(self, phase_init):
         self.phase = phase_init
         if self.phase == "swing":
+            self.x = 0
             self.y = 0
             self.z = 0
         elif self.phase == "support":
+            self.x = 0
             self.y = STEP_LENGTH
             self.z = 0
         self.interpolations = 40
@@ -43,17 +46,20 @@ class Trajectory:
             if self.y >= STEP_LENGTH:
                 self.y = STEP_LENGTH
                 self.phase = "support"
-            self.z = STEP_HEIGHT * math.sin(2 * math.pi / (STEP_LENGTH * 2) * self.y)
+            self.z = STEP_HEIGHT * math.sin(2 * math.pi / (STEP_LENGTH * 2) * self.y) - GROUND_DEPTH
         elif self.phase == "support":
             self.y -= factor
             if self.y <= 0:
                 self.y = 0
                 self.phase = "swing"
-            self.z = -BACK_STEP_DEPTH * math.sin(2 * math.pi / (STEP_LENGTH * 2) * self.y)
+            self.z = -BACK_STEP_DEPTH * math.sin(2 * math.pi / (STEP_LENGTH * 2) * self.y) - GROUND_DEPTH
+    def get_x(self):
+        return self.x
     def get_y(self):
         return self.y
     def get_z(self):
         return self.z
+    
     
 def solve_IK(targetPos, abadPos): 
     x = targetPos[0]-abadPos[0]
@@ -62,12 +68,15 @@ def solve_IK(targetPos, abadPos):
     l = math.sqrt(x**2 + y**2 + z**2)
     d = math.sqrt(l**2 - L3**2)
     alpha = math.acos((L1**2 + d**2 - L2**2) / (2 * L1 * d))
-    beta = math.asin(abs(y) / d)
+    beta = math.asin(-y / d)
     theta = math.pi/2 - alpha - beta
     row = math.acos((L1**2 + L2**2 - d**2) / (2 * L1 * L2))
     phi = row - theta
     sigma = math.acos(L3/math.sqrt(x**2 + z**2))
-    omega = sigma - math.atan(abs(z)/abs(x))
+    try:
+        omega = sigma - math.atan(-z/abs(x))
+    except ZeroDivisionError:
+        omega = 0
     return [omega, theta, phi] #abad angle, hip angle, knee angle
 
     
@@ -86,10 +95,10 @@ def animate(frame):
     BR_trajectory.interpolate_next(GAIT_SPEED)
     BL_trajectory.interpolate_next(GAIT_SPEED)
     
-    FR_footPos = translateTrajectory([0, FR_trajectory.get_y(), FR_trajectory.get_z()], "FR")
-    FL_footPos = translateTrajectory([0, FL_trajectory.get_y(), FL_trajectory.get_z()], "FL")
-    BR_footPos = translateTrajectory([0, BR_trajectory.get_y(), BR_trajectory.get_z()], "BR")
-    BL_footPos = translateTrajectory([0, BL_trajectory.get_y(), BL_trajectory.get_z()], "BL")
+    FR_footPos = translateTrajectory([FR_trajectory.get_x(), FR_trajectory.get_y(), FR_trajectory.get_z()], "FR")
+    FL_footPos = translateTrajectory([FL_trajectory.get_x(), FL_trajectory.get_y(), FL_trajectory.get_z()], "FL")
+    BR_footPos = translateTrajectory([BR_trajectory.get_x(), BR_trajectory.get_y(), BR_trajectory.get_z()], "BR")
+    BL_footPos = translateTrajectory([BL_trajectory.get_x(), BL_trajectory.get_y(), BL_trajectory.get_z()], "BL")
     
     FR_angles = solve_IK(FR_footPos, FR_abadPos)
     FL_angles = solve_IK(FL_footPos, FL_abadPos)
@@ -98,9 +107,9 @@ def animate(frame):
     
     #forward kinematics to find hip joint pos
     FR_hipPos = [FR_abadPos[0] + L3*math.cos(FR_angles[0]), FR_abadPos[1], FR_abadPos[2] + L3*math.sin(FR_angles[0])]
-    FL_hipPos = [FL_abadPos[0] - L3*math.cos(FL_angles[0]), FL_abadPos[1], FL_abadPos[2] - L3*math.sin(FL_angles[0])]
+    FL_hipPos = [FL_abadPos[0] - L3*math.cos(FL_angles[0]), FL_abadPos[1], FL_abadPos[2] + L3*math.sin(FL_angles[0])]
     BR_hipPos = [BR_abadPos[0] + L3*math.cos(BR_angles[0]), BR_abadPos[1], BR_abadPos[2] + L3*math.sin(BR_angles[0])]
-    BL_hipPos = [BL_abadPos[0] - L3*math.cos(BL_angles[0]), BL_abadPos[1], BL_abadPos[2] - L3*math.sin(BL_angles[0])]
+    BL_hipPos = [BL_abadPos[0] - L3*math.cos(BL_angles[0]), BL_abadPos[1], BL_abadPos[2] + L3*math.sin(BL_angles[0])]
     
     #draw pelvis (bone that connects abad to hip joint)
     ax.plot([FR_abadPos[0], FR_hipPos[0]], [FR_abadPos[1], FR_hipPos[1]], [FR_abadPos[2], FR_hipPos[2]])
@@ -126,24 +135,22 @@ def animate(frame):
     ax.plot([BR_kneePos[0], BR_footPos[0]], [BR_kneePos[1], BR_footPos[1]], [BR_kneePos[2], BR_footPos[2]])
     ax.plot([BL_kneePos[0], BL_footPos[0]], [BL_kneePos[1], BL_footPos[1]], [BL_kneePos[2], BL_footPos[2]])
     
-    
-    #ax.plot([FR_trajectory[0], ], [FR_trajectory[1], ], [FR_trajectory[2], ])
-    
-    ax.plot([0, FR_footPos[0]], [0, FR_footPos[1]], [0, FR_footPos[2]])
-    ax.plot([0, FL_footPos[0]], [0, FL_footPos[1]], [0, FL_footPos[2]])
-    ax.plot([0, BR_footPos[0]], [0, BR_footPos[1]], [0, BR_footPos[2]])
-    ax.plot([0, BL_footPos[0]], [0, BL_footPos[1]], [0, BL_footPos[2]])
+    #draw test line from origin to foot position
+    # ax.plot([0, FR_footPos[0]], [0, FR_footPos[1]], [0, FR_footPos[2]])
+    # ax.plot([0, FL_footPos[0]], [0, FL_footPos[1]], [0, FL_footPos[2]])
+    # ax.plot([0, BR_footPos[0]], [0, BR_footPos[1]], [0, BR_footPos[2]])
+    # ax.plot([0, BL_footPos[0]], [0, BL_footPos[1]], [0, BL_footPos[2]])
     
     
 def translateTrajectory(pos, leg):
     if leg == "FR":
-        return [FR_abadPos[0]+50, pos[1]+FR_abadPos[1]-STEP_LENGTH/2, pos[2]-GROUND_DEPTH]
+        return [FR_abadPos[0]+50, pos[1]+FR_abadPos[1]-STEP_LENGTH/2, pos[2]]
     elif leg == "FL":
-        return [FL_abadPos[0]-50, pos[1]+FL_abadPos[1]-STEP_LENGTH/2, pos[2]-GROUND_DEPTH]
+        return [FL_abadPos[0]-50, pos[1]+FL_abadPos[1]-STEP_LENGTH/2, pos[2]]
     elif leg == "BR":
-        return [BR_abadPos[0]+50, pos[1]+BR_abadPos[1]-STEP_LENGTH/2, pos[2]-GROUND_DEPTH]
+        return [BR_abadPos[0]+50, pos[1]+BR_abadPos[1]-STEP_LENGTH/2, pos[2]]
     elif leg == "BL":
-        return [BL_abadPos[0]-50, pos[1]+BL_abadPos[1]-STEP_LENGTH/2, pos[2]-GROUND_DEPTH]
+        return [BL_abadPos[0]-50, pos[1]+BL_abadPos[1]-STEP_LENGTH/2, pos[2]]
     return
 
 #main program below
