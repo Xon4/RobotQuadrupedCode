3b37f0e3226ce291de9965ca46d181588b9dafee
Andrew Cao <xoncao@gmail.com>
2024-08-12 17:01:19 -0700

    working trot gait

--- src/simulation.py
+++ src/simulation.py
@@ -8,6 +8,7 @@ STEP_LENGTH = 100
 STEP_HEIGHT = 60
 BACK_STEP_DEPTH = 10
 GROUND_DEPTH = 150
+GAIT_SPEED = 100
 
 FR_abadPos = [50, 150, 0]
 FL_abadPos = [-50, 150, 0]
@@ -54,21 +55,20 @@ class Trajectory:
     def get_z(self):
         return self.z
     
-def solve_IK(targetPos, abadPos):
+def solve_IK(targetPos, abadPos): 
     x = targetPos[0]-abadPos[0]
     y = targetPos[1]-abadPos[1]
     z = targetPos[2]-abadPos[2]
-    d = math.sqrt(math.pow(y, 2) + math.pow(z, 2))
-    alpha = math.acos((math.pow(L1, 2) + math.pow(L2, 2) - math.pow(d, 2)) / (2 * L1 * L2))
-    theta = math.pi/2 + math.atan(y / z) - math.asin(L2 * math.sin(alpha) / d)
-    phi = alpha - theta
-    row = math.asin(L3 / math.sqrt(math.pow(z, 2) + pow(x, 2)))
-    try: 
-        omega = row - math.atan(z / x)
-    except ZeroDivisionError:
-        omega = row - math.pi/2
-    
-    return omega, theta, phi #abad angle, hip angle, knee angle
+    l = math.sqrt(x**2 + y**2 + z**2)
+    d = math.sqrt(l**2 - L3**2)
+    alpha = math.acos((L1**2 + d**2 - L2**2) / (2 * L1 * d))
+    beta = math.asin(abs(y) / d)
+    theta = math.pi/2 - alpha - beta
+    row = math.acos((L1**2 + L2**2 - d**2) / (2 * L1 * L2))
+    phi = row - theta
+    sigma = math.acos(L3/math.sqrt(x**2 + z**2))
+    omega = sigma - math.atan(abs(z)/abs(x))
+    return [omega, theta, phi] #abad angle, hip angle, knee angle
 
     
 def animate(frame):
@@ -81,39 +81,58 @@ def animate(frame):
     ax.set_zlabel('Z axis')
     
     drawBody() 
-    FR_trajectory.interpolate_next(100)
-    FL_trajectory.interpolate_next(100)
-    BR_trajectory.interpolate_next(100)
-    BL_trajectory.interpolate_next(100)
+    FR_trajectory.interpolate_next(GAIT_SPEED)
+    FL_trajectory.interpolate_next(GAIT_SPEED)
+    BR_trajectory.interpolate_next(GAIT_SPEED)
+    BL_trajectory.interpolate_next(GAIT_SPEED)
     
-    FR_targetPos = translateTrajectory([0, FR_trajectory.get_y(), FR_trajectory.get_z()], "FR")
-    FL_targetPos = translateTrajectory([0, FL_trajectory.get_y(), FL_trajectory.get_z()], "FL")
-    BR_targetPos = translateTrajectory([0, BR_trajectory.get_y(), BR_trajectory.get_z()], "BR")
-    BL_targetPos = translateTrajectory([0, BL_trajectory.get_y(), BL_trajectory.get_z()], "BL")
+    FR_footPos = translateTrajectory([0, FR_trajectory.get_y(), FR_trajectory.get_z()], "FR")
+    FL_footPos = translateTrajectory([0, FL_trajectory.get_y(), FL_trajectory.get_z()], "FL")
+    BR_footPos = translateTrajectory([0, BR_trajectory.get_y(), BR_trajectory.get_z()], "BR")
+    BL_footPos = translateTrajectory([0, BL_trajectory.get_y(), BL_trajectory.get_z()], "BL")
     
-    FR_angles = solve_IK(FR_targetPos, FR_abadPos)
-    FL_angles = solve_IK(FL_targetPos, FL_abadPos)
-    BR_angles = solve_IK(BR_targetPos, BR_abadPos)
-    BL_angles = solve_IK(BL_targetPos, BL_abadPos)
+    FR_angles = solve_IK(FR_footPos, FR_abadPos)
+    FL_angles = solve_IK(FL_footPos, FL_abadPos)
+    BR_angles = solve_IK(BR_footPos, BR_abadPos)
+    BL_angles = solve_IK(BL_footPos, BL_abadPos)
     
     #forward kinematics to find hip joint pos
-    FR_hipPos = [FR_abadPos[0] - L3*math.cos(FR_angles[0]), FR_abadPos[1], FR_abadPos[2] + L3*math.sin(FR_angles[0])]
-    FL_hipPos = [FL_abadPos[0], FL_abadPos[1] + L3*math.sin(FL_angles[0]), FL_abadPos[2] - L3*math.cos(FL_angles[0])]
-    BR_hipPos = [BR_abadPos[0], BR_abadPos[1] + L3*math.sin(BR_angles[0]), BR_abadPos[2] - L3*math.cos(BR_angles[0])]
-    BL_hipPos = [BL_abadPos[0], BL_abadPos[1] + L3*math.sin(BL_angles[0]), BL_abadPos[2] - L3*math.cos(BL_angles[0])]
+    FR_hipPos = [FR_abadPos[0] + L3*math.cos(FR_angles[0]), FR_abadPos[1], FR_abadPos[2] + L3*math.sin(FR_angles[0])]
+    FL_hipPos = [FL_abadPos[0] - L3*math.cos(FL_angles[0]), FL_abadPos[1], FL_abadPos[2] - L3*math.sin(FL_angles[0])]
+    BR_hipPos = [BR_abadPos[0] + L3*math.cos(BR_angles[0]), BR_abadPos[1], BR_abadPos[2] + L3*math.sin(BR_angles[0])]
+    BL_hipPos = [BL_abadPos[0] - L3*math.cos(BL_angles[0]), BL_abadPos[1], BL_abadPos[2] - L3*math.sin(BL_angles[0])]
     
-    #draw pelvis
+    #draw pelvis (bone that connects abad to hip joint)
     ax.plot([FR_abadPos[0], FR_hipPos[0]], [FR_abadPos[1], FR_hipPos[1]], [FR_abadPos[2], FR_hipPos[2]])
     ax.plot([FL_abadPos[0], FL_hipPos[0]], [FL_abadPos[1], FL_hipPos[1]], [FL_abadPos[2], FL_hipPos[2]])
     ax.plot([BR_abadPos[0], BR_hipPos[0]], [BR_abadPos[1], BR_hipPos[1]], [BR_abadPos[2], BR_hipPos[2]])
     ax.plot([BL_abadPos[0], BL_hipPos[0]], [BL_abadPos[1], BL_hipPos[1]], [BL_abadPos[2], BL_hipPos[2]])
     
+    #forward kinematics to find knee joint pos
+    FR_kneePos = [FR_hipPos[0] + L1*math.sin(FR_angles[1])*math.sin(FR_angles[0]), FR_hipPos[1] - L1*math.cos(FR_angles[1]), FR_hipPos[2] - L1*math.sin(FR_angles[1])*math.cos(FR_angles[0])]
+    FL_kneePos = [FL_hipPos[0] + L1*math.sin(FL_angles[1])*math.sin(FL_angles[0]), FL_hipPos[1] - L1*math.cos(FL_angles[1]), FL_hipPos[2] - L1*math.sin(FL_angles[1])*math.cos(FL_angles[0])]
+    BR_kneePos = [BR_hipPos[0] + L1*math.sin(BR_angles[1])*math.sin(BR_angles[0]), BR_hipPos[1] - L1*math.cos(BR_angles[1]), BR_hipPos[2] - L1*math.sin(BR_angles[1])*math.cos(BR_angles[0])]
+    BL_kneePos = [BL_hipPos[0] + L1*math.sin(BL_angles[1])*math.sin(BL_angles[0]), BL_hipPos[1] - L1*math.cos(BL_angles[1]), BL_hipPos[2] - L1*math.sin(BL_angles[1])*math.cos(BL_angles[0])]
+    
+    #draw femur (bone that connects hip to knee joint)
+    ax.plot([FR_kneePos[0], FR_hipPos[0]], [FR_kneePos[1], FR_hipPos[1]], [FR_kneePos[2], FR_hipPos[2]])
+    ax.plot([FL_kneePos[0], FL_hipPos[0]], [FL_kneePos[1], FL_hipPos[1]], [FL_kneePos[2], FL_hipPos[2]])
+    ax.plot([BR_kneePos[0], BR_hipPos[0]], [BR_kneePos[1], BR_hipPos[1]], [BR_kneePos[2], BR_hipPos[2]])
+    ax.plot([BL_kneePos[0], BL_hipPos[0]], [BL_kneePos[1], BL_hipPos[1]], [BL_kneePos[2], BL_hipPos[2]])
+    
+    #draw tibia (bone that connects knee to foot joint)
+    ax.plot([FR_kneePos[0], FR_footPos[0]], [FR_kneePos[1], FR_footPos[1]], [FR_kneePos[2], FR_footPos[2]])
+    ax.plot([FL_kneePos[0], FL_footPos[0]], [FL_kneePos[1], FL_footPos[1]], [FL_kneePos[2], FL_footPos[2]])
+    ax.plot([BR_kneePos[0], BR_footPos[0]], [BR_kneePos[1], BR_footPos[1]], [BR_kneePos[2], BR_footPos[2]])
+    ax.plot([BL_kneePos[0], BL_footPos[0]], [BL_kneePos[1], BL_footPos[1]], [BL_kneePos[2], BL_footPos[2]])
+    
+    
     #ax.plot([FR_trajectory[0], ], [FR_trajectory[1], ], [FR_trajectory[2], ])
     
-    ax.plot([0, FR_targetPos[0]], [0, FR_targetPos[1]], [0, FR_targetPos[2]])
-    ax.plot([0, FL_targetPos[0]], [0, FL_targetPos[1]], [0, FL_targetPos[2]])
-    ax.plot([0, BR_targetPos[0]], [0, BR_targetPos[1]], [0, BR_targetPos[2]])
-    ax.plot([0, BL_targetPos[0]], [0, BL_targetPos[1]], [0, BL_targetPos[2]])
+    ax.plot([0, FR_footPos[0]], [0, FR_footPos[1]], [0, FR_footPos[2]])
+    ax.plot([0, FL_footPos[0]], [0, FL_footPos[1]], [0, FL_footPos[2]])
+    ax.plot([0, BR_footPos[0]], [0, BR_footPos[1]], [0, BR_footPos[2]])
+    ax.plot([0, BL_footPos[0]], [0, BL_footPos[1]], [0, BL_footPos[2]])
     
     
 def translateTrajectory(pos, leg):
